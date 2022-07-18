50448ef528b852f95c5575fbef7d1c716eefd632
雪枚 <1659513605@qq.com>
2022-07-18 23:47:15 +0800

    countover

--- yiyi-count/src/components/home.vue
+++ yiyi-count/src/components/home.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="yiyi">
-    <span @click="isgo=1">加</span> <span @click="isgo=2">减</span> <span @click="isgo=3">乘</span> <span @click="isgo=4">除</span>
+    <span @click="isgo=1" :style="{ opacity : isgo ===1 ? '0.8 ': '0.5'}">加</span> <span @click="isgo=2" :style="{ opacity : isgo ===2 ? '0.8 ': '0.5'}">减</span> <span @click="isgo=3" :style="{ opacity : isgo ===3 ? '0.8 ': '0.5'}">乘</span> <span @click="isgo=4" :style="{ opacity : isgo ===5 ? '0.8 ': '0.5'}">除</span>
 <!--    <span @click="isgo=5" class="yiImg"><img src="../../src/assets/yiyi.png"></span>-->
   </div>
+  <div class="over"><span>总完成:{{over}}</span><span>  + : {{jiaOver}}</span><span>  - : {{jieOver}}</span><span>  * : {{chenOver}}</span><span>  / : {{chuOver}}</span></div>
   <div class="k"></div>
-  <div v-show="isgo==1"><Jia  ></Jia></div>
-  <div v-show="isgo===2"><Jie  ></Jie></div>
-  <div v-show="isgo===3"><Chen ></Chen></div>
-  <div  v-show="isgo===4"><Chu ></Chu></div>
+  <div v-show="isgo===1"><Jia @overjia="showOverJia" ></Jia></div>
+  <div v-show="isgo===2"><Jie  @overjie="showOverJie"></Jie></div>
+  <div v-show="isgo===3"><Chen @overchen="showOverchen"></Chen></div>
+  <div  v-show="isgo===4"><Chu @overchu="showOverchu"></Chu></div>
 <!--  <div v-show="isgo===5"><My></My></div>-->
 
 </template>
@@ -19,18 +20,40 @@ import Chen from "../components/chen"
 import Chu from "../components/chu"
 // import My from "../components/my"
 
-import {reactive,toRefs} from 'vue'
+import {reactive,toRefs,onUpdated} from 'vue'
 export default {
   name: 'Home',
   components: {
     Jia,Jie,Chen,Chu
   },
   setup(){
-    let h= reactive({
-        isgo:1
 
+    let h= reactive({
+        isgo:1,
+        over:0,
+        jiaOver:0,
+        jieOver:0,
+        chenOver:0,
+        chuOver:0,
         }
     )
+//完成数量
+     h.showOverJia=(v)=>{
+       h.jiaOver=v
+    }
+    h.showOverJie=(v)=>{
+      h.jieOver=v
+    }
+    h.showOverchen=(v)=>{
+      h.chenOver=v
+    }
+    h.showOverchu=(v)=>{
+      h.chuOver=v
+    }
+    onUpdated(()=>{
+      // 完成总数
+      h.over=h.jieOver+h.jiaOver+h.chenOver+h.chuOver
+    })
     return{
       ...toRefs(h)
     }
@@ -46,7 +69,20 @@ export default {
   font-size: 20px;
 }
 .k{
-  height: 50px;
+  height: 90px;
+}
+.over {
+  border: 1px solid lightseagreen;
+  width: 100%;
+  position: fixed;
+  z-index: 9999999;
+  background-color: #f7d8d8;
+  top: 55px;
+  display: flex;
+  justify-content: space-evenly;
+}
+.over span{
+  text-align: center;
 }
 .yiyi{
   display: flex;
