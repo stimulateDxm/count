367b83608a021b2d9cf77d6ff046040df199b221
雪枚 <1659513605@qq.com>
2022-07-29 22:44:41 +0800

    shan

---
+++ shanjias/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<Tests></Tests>
+</template>
+
+<script>
+import Tests from "../src/components/tests"
+export default {
+  name: 'App',
+  components: {
+    Tests
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
